0073ec0cedb2db7949bd370b02caac04fe0382cf
Kaygisiz02 <bektasbaykara@gmail.com>
2024-06-10 23:30:59 +0300

    viewcomponedentegrasyonu_future

---
+++ MultiShop.Presentation/Views/Shared/Components/TopBar/TopBarViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.Presentation.Views.Shared.Components.TopBar
+{
+    public class TopBarViewComponent : ViewComponent
+    {    //TopBar
+            public IViewComponentResult Invoke()
+            {
+                return View();
+            }
+    }
+}
